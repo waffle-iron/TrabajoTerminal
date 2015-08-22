6ac323f8387741cc83a9ef39df40d5859002633d
Yair <yayo_500@hotmail.com>
2015-08-22 15:29:46 -0500

    #2, CRUD completo de la relacion Nivel

--- src/main/java/com/escom/tt/controlador/NivelContralador.java
+++ src/main/java/com/escom/tt/controlador/NivelContralador.java
@@ -1,6 +1,8 @@
 package com.escom.tt.controlador;
 
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,7 +26,7 @@ public class NivelContralador {
 	@RequestMapping(value="/nivel/crear", method = RequestMethod.GET)
 	public String crear(Model modelo){
 		modelo.addAttribute("nivel", new Nivel());
-		return "idioma-crear";
+		return "nivel-crear";
 	}
 	
 	@RequestMapping(value="/nivel/guardar", method = RequestMethod.POST)
@@ -38,10 +41,73 @@ public class NivelContralador {
 		return ruta;
 	}
 	
+	@RequestMapping(value="/nivel/guardarCambios", method = RequestMethod.POST)
+	public String guardarCambios(@ModelAttribute("nivel") @Valid Nivel nivel, BindingResult validacion, Model modelo){
+		String ruta = null;
+		
+		if (validacion.hasErrors()){
+			modelo.addAttribute("nivel", nivel);
+			ruta = "nivel-editar";
+		}else{
+			Integer id = nivelRepositorio.actualizarNivel(nivel);
+			ruta = "redirect:/nivel/ver/" + nivel.getIdNivel() + "/?actualizado=true";
+		}
+		return ruta;
+			
+		}
 	
+	@RequestMapping(value="/nivel/{nivelId:[0-9]+}/editar", method = RequestMethod.GET)
+	public String editar(@PathVariable Integer nivelId, Model modelo){
+		String ruta = null;
+		Nivel nivel = null;
+		nivel = nivelRepositorio.buscarPorId(nivelId);
+		
+		if (nivel !=null){
+			modelo.addAttribute("nivel", nivel);
+			ruta = "nivel-editar";
+		}else{
+			ruta = "redirect:/nivel";
+		}
+		
+		
+		return ruta;
+	}
 	
+	@RequestMapping(value="/nivel/ver/{nivelId:[0-9]+}")
+	public String ver(@PathVariable Integer nivelId, Model modelo, Boolean actualizado, Boolean creado) {
+		String ruta = null;
+		Nivel nivel = null;
+		nivel = nivelRepositorio.buscarPorId(nivelId);
+		
+		if(nivel != null){
+			modelo.addAttribute("nivel", nivel);
+			modelo.addAttribute("actualizado", actualizado);
+			modelo.addAttribute("creado", creado);
+			ruta = "nivel-ver";
+		}else{
+			ruta = "redirect:/nivel";
+		}
+		return ruta;
+	}
 	
-	
-	
-
+	@RequestMapping(value="/nivel/eliminar/{nivelId:[0-9]+}")
+	public String eliminar (@PathVariable Integer nivelId, Model modelo){
+		nivelRepositorio.eliminarNivel(nivelRepositorio.buscarPorId(nivelId));
+		modelo.addAttribute("mensaje", "se ha eliminado el nivel");
+		return "nivel-eliminar";
+		
+	}
+	@RequestMapping(value = "/nivel")
+	public String verTodos(Model modelo){
+		List<Nivel> niveles = null;
+		niveles= nivelRepositorio.obtenerTodos();
+		modelo.addAttribute("niveles", niveles);
+		return "nivel-todos";
+				
+	}
 }
+	
+	
+	
+	
+	
